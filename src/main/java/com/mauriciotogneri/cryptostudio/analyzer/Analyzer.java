4e545866d92733ccd9ab153ea621adfec8ece3f5
mtogneri <mtogneri@openwt.com>
2018-01-28 23:24:24 +0100

    Code cleanup

--- src/main/java/com/mauriciotogneri/cryptostudio/analyzer/Analyzer.java
+++ src/main/java/com/mauriciotogneri/cryptostudio/analyzer/Analyzer.java
@@ -1,11 +1,9 @@
 package com.mauriciotogneri.cryptostudio.analyzer;
 
 import com.mauriciotogneri.cryptostudio.model.CandleStick;
-import com.mauriciotogneri.cryptostudio.result.Event;
 import com.mauriciotogneri.cryptostudio.result.Result;
 
 import java.util.List;
-import java.util.Random;
 
 public class Analyzer
 {
@@ -14,14 +12,10 @@ public class Analyzer
         Result result = new Result(parameters);
 
         List<CandleStick> candleSticks = parameters.candleSticks();
-        Random random = new Random();
 
         for (CandleStick candleStick : candleSticks)
         {
-            if (random.nextInt(1000) == 0)
-            {
-                result.event(new Event());
-            }
+            // TODO
         }
 
         return result;
