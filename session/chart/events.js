e2fd6ad5596a042fe11b7ee0916af33aa6f1f47a
mtogneri <mtogneri@openwt.com>
2018-02-04 15:05:53 +0100

    Refactored chart

---
+++ session/chart/events.js
@@ -0,0 +1,52 @@
+function isValidEvent(event)
+{
+    return (event.type == 'buy') || (event.type == 'sale')
+}
+
+function eventTitle(event)
+{
+    if (event.type == 'watching_buy')
+    {
+        return 'WB'
+    }
+    else if (event.type == 'trailing_buy')
+    {
+        return 'TB'
+    }
+    else if (event.type == 'buy')
+    {
+        return 'B'
+    }
+    else if (event.type == 'watching_sell')
+    {
+        return 'WS'
+    }
+    else if (event.type == 'trailing_sell')
+    {
+        return 'TS'
+    }
+    else if (event.type == 'sale')
+    {
+        return 'S'
+    }
+    else
+    {
+        return event.type
+    }
+}
+
+function eventText(event)
+{
+    if (event.type == 'buy')
+    {
+        return '<b>' + event.type.replace('_', ' ') + '</b><br>price: ' + event.price + '<br>amount: ' + event.amount + '<br>total: ' + event.total
+    }
+    else if (event.type == 'sale')
+    {
+        return '<b>' + event.type.replace('_', ' ') + '</b><br>price: ' + event.price + '<br>amount: ' + event.amount + '<br>total: ' + event.total + '<br>profit total: ' + event.profitTotal + '<br>profit percentage: ' + event.profitPercentage + '%'
+    }
+    else
+    {
+        return '<b>' + event.type.replace('_', ' ') + ':</b> ' + event.price
+    }
+}
