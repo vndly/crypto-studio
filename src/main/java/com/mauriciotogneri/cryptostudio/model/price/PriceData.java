dbba9ff842a238f8a6355aa99ee09fe27dc3de9c
mtogneri <mtogneri@openwt.com>
2018-02-04 16:50:36 +0100

    Code cleanup

--- src/main/java/com/mauriciotogneri/cryptostudio/model/price/PriceData.java
+++ src/main/java/com/mauriciotogneri/cryptostudio/model/price/PriceData.java
@@ -1,30 +1,18 @@
 package com.mauriciotogneri.cryptostudio.model.price;
 
-public class PriceData
+public class PriceData extends PriceHistory
 {
-    private final long time;
-    private final double price;
     private final double sma1;
     private final double sma2;
     private final double last24H;
 
     public PriceData(long time, double price, double sma1, double sma2, double last24H)
     {
-        this.time = time;
-        this.price = price;
+        super(time, price);
+
         this.sma1 = sma1;
         this.sma2 = sma2;
         this.last24H = last24H;
-    }
-
-    public long time()
-    {
-        return time;
-    }
-
-    public double price()
-    {
-        return price;
     }
 
     public double sma1()
